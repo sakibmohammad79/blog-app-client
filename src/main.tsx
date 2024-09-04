e9e3b05b2afd84306d00ab6709f8983fec45f2f5
sakib <mohammadsakib7679@gmail.com>
2024-09-04 18:13:49 +0600

    post data get using graphql apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Router.tsx";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <ApolloProvider client={client}>
+      <RouterProvider router={router}></RouterProvider>
+    </ApolloProvider>
   </StrictMode>
 );
--- src/pages/Blog/BlogPage.tsx
+++ src/pages/Blog/BlogPage.tsx
@@ -1,7 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query posts {
+    posts {
+      content
+      id
+      createdAt
+      author {
+        name
+        email
+        id
+      }
+    }
+  }
+`;
+
 const BlogPage = () => {
+  const { data, loading, error } = useQuery(GET_POSTS);
+  console.log(data);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
   return (
     <div>
-      <h2>This is BlogPage component</h2>
+      {data?.posts.map((post) => (
+        <div>
+          <h2>content: {post.content}</h2>
+          <h2>Author: {post.author.name}</h2>
+        </div>
+      ))}
     </div>
   );
 };
